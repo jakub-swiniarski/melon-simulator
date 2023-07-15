07f9ded423420e6f0d3ecf9e2735603f35ed9e61
Jakub Świniarski <jakub.swiniarski2007@gmail.com>
2023-07-15 21:35:58 +0200

    initial commit v2

---
+++ desktop/src/com/jswin/melon/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.jswin.melon;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.jswin.melon.MelonSimulator;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Melon Simulator");
+		new Lwjgl3Application(new MelonSimulator(), config);
+	}
+}

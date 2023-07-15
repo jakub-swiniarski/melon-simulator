acd4919bbd3f1379140119fb34bfcbbe59942eb2
Jakub Świniarski <jakub.swiniarski2007@gmail.com>
2023-07-15 22:31:29 +0200

    draw melon

--- core/src/com/jswin/melon/Melon.java
+++ core/src/com/jswin/melon/Melon.java
@@ -1,17 +1,32 @@
 package com.jswin.melon;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Melon {
-    Rectangle rect;
-    Texture img;
+    private Rectangle rect;
+    private Texture img;
 
-    Melon(){
-
+    public Melon(){
+        img=new Texture(Gdx.files.internal("melon.png"));
+        rect=new Rectangle();
+        rect.width=104;
+        rect.height=114;
+        rect.x=100;
+        rect.y=100;
     }
 
-    void update(){
+    public void update(SpriteBatch batch){
+        batch.draw(img,rect.x,rect.y);
+    }
+
+    public void dispose(){
+        img.dispose();
+    }
+
+    public void checkForCollisions(){
 
     }
 }
--- core/src/com/jswin/melon/MelonSimulator.java
+++ core/src/com/jswin/melon/MelonSimulator.java
@@ -7,22 +7,25 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MelonSimulator extends ApplicationAdapter {
 	SpriteBatch batch;
-	
+	Melon m;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		m=new Melon();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-
+		m.update(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
+		m.dispose();
 	}
 }

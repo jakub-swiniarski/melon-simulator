ab723659962a2b4bccc2fb333c363f23e679670e
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-07-15 23:33:05 +0200

    removed unused imports

--- core/src/com/jswin/melon/MelonSimulator.java
+++ core/src/com/jswin/melon/MelonSimulator.java
@@ -3,12 +3,8 @@ package com.jswin.melon;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.util.ArrayList;
@@ -19,7 +15,6 @@ public class MelonSimulator extends ApplicationAdapter {
 	boolean fullscreen;
 	List<Melon> melon;
 	Vector3 mousePos;
-	//World world;
 
 	//REMEMBER TO ADD DELTA TIME MOVEMENT
 
@@ -28,7 +23,6 @@ public class MelonSimulator extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		fullscreen=false;
 
-		//world = new World(new Vector2(0, -10), true);
 		melon=new ArrayList<Melon>();
 	}
 

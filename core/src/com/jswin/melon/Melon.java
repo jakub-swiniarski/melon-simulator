472bfd3fc6985653b35b5a66b77079cbb6c1ae70
Jakub Świniarski <jakub.swiniarski2007@gmail.com>
2023-07-15 22:13:34 +0200

    melon class

---
+++ core/src/com/jswin/melon/Melon.java
@@ -0,0 +1,17 @@
+package com.jswin.melon;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Melon {
+    Rectangle rect;
+    Texture img;
+
+    Melon(){
+
+    }
+
+    void update(){
+
+    }
+}
